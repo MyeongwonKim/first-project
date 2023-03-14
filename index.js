7804cf9acec1f3032722402eab1b810bf7823026
MyeongwonKim <mw7539@naver.com>
2023-03-14 11:53:08 +0900

    구구단 프로그램 추가
    1부터 9까지 각각 곱한 결과를 출력하는 파일을 추가함

---
+++ index.js
@@ -0,0 +1,11 @@
+function printMultiplicationTable() {
+    for (let i = 1; i <= 9; i++) {
+        let row = "";
+        for (let j = 1; j <= 9; j++) {
+        row += `${i} x ${j} = ${i * j}\t`;
+        }
+        console.log(row);
+    }
+}
+
+printMultiplicationTable();
